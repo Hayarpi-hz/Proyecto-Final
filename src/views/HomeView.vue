380be8def5d14f4f57390bbfc9138e9c2d63b1d1
Hayarpi-hz <hayarpi@amazon.com>
2025-05-31 00:30:38 +0200

    Logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,36 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 
 <template>
 <HeaderComponent />
-<h2>Home</h2>
+<div class="logo-container">
+    <!-- Aquí puedes utilizar una imagen si tienes un logo gráfico -->
+    <!-- <img src="../assets/logo.png" alt="DIY Logo" class="diy-logo" /> -->
+    
+    <!-- Texto de tu logo -->
+    <h1 class="diy-text">I want it, I make it</h1>
+  </div>
 
 </template>
 
 
-<style>
+<style scoped>
+.logo-container {
+  display: flex;
+  flex-direction: column; /* Para alinear el texto e imagen verticalmente */
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Ocupa toda la altura de la página */
+  text-align: center;
+}
 
+.diy-text {
+  font-family: 'Arial', sans-serif;
+  font-size: 3rem;
+  color: #333; /* Cambia el color según tu diseño */
+  margin: 0;
+}
+
+.diy-logo {
+  width: 150px; /* Ajusta el tamaño del logo según necesites */
+  margin-bottom: 20px; /* Espacio entre la imagen y el texto */
+}
 </style>
